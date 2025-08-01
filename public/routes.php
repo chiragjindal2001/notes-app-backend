2c125da790c08272f0c0b680c66ea3a5cedd06f8
chiragjindal2001 <chiragjin27@gmail.com>
2025-08-02 02:12:56 +0530

    Update routes.php

--- public/routes.php
+++ public/routes.php
@@ -1,11 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+
 // All routing logic moved here from index.php
 $uri = strtok($_SERVER['REQUEST_URI'], '?');
 echo $uri;
